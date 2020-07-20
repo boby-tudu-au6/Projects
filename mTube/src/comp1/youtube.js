420fdd867d8b127938e08e1143edf79a6593cd8d
bobytudu <ajabdas999@gmail.com>
2020-07-20 19:51:45 +0530

    mtube modified

--- mTube/src/comp1/youtube.js
+++ mTube/src/comp1/youtube.js
@@ -1,22 +1,3 @@
 export const cred = {
-api_key:'AIzaSyCcpuuptF6WeBkP3sFeQj6Xf9GaWTJIIsQ',
-client_id:'175770290389-i75dl55o0vgkqmtaqqk4r553sij2tjnk.apps.googleusercontent.com',
-    scope:'https://www.googleapis.com/auth/youtube.readonly',
-    doc:'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
+api_key:process.env.API_KEY,
 }
-
-// attainu
-// api_key:"AIzaSyDiFpSSg74jzMz61ulV_JEIiHNL1g02xfw",
-// client_id:"384190643071-le1eehpe1guodg8n6q9s4vv2f97ahuah.apps.googleusercontent.com",
-
-// js90
-// api_key:'AIzaSyA6nEOSx0BLGwKNHwl_fNPFKyotcgdUezs',
-// client_id:'175770290389-i75dl55o0vgkqmtaqqk4r553sij2tjnk.apps.googleusercontent.com',
-
-// manas
-// api_key:'gDbxIqR8SHcJWpYAuJJCMW9h',
-// client_id:"http://154727329238-sjjbbslsckjuofhs94k71g6nvaiuru4k.apps.googleusercontent.com/"
-
-// ajab
-// api_key:'AIzaSyCcpuuptF6WeBkP3sFeQj6Xf9GaWTJIIsQ',
-// client_id:"169104568545-2kmp2h3cpmvf3tfvf61u9o0g2j8qokkj.apps.googleusercontent.com"
